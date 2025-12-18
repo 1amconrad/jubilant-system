7fc2eac277dd8f784abd9dd45dec16b4c364ab34
1amconrad <gustavooli0082@gmail.com>
2025-12-18 20:10:26 -0300

    Fix formatting and quotes in config.js

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // SUBSTITUA COM SUAS CHAVES DO SUPABASE (Configurações -> API)
-const supabaseUrl = https://ptxvudozgamnoapywclo.supabase.co
+const supabaseUrl = 'https://ptxvudozgamnoapywclo.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0eHZ1ZG96Z2Ftbm9hcHl3Y2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwOTA2MTUsImV4cCI6MjA4MTY2NjYxNX0.oNWlHaWj6_pgar7olXaanpIcSxk2XZ_Ghj8r7ozdGKo'
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
